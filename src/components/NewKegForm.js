03a35e62a6084f3a899671a7fc0857b62f6319c0
TorchAblaze <tiffanygreathead@gmail.com>
2021-07-02 14:03:18 -0700

    Add new Keg Form input functionality

--- src/components/NewKegForm.js
+++ src/components/NewKegForm.js
@@ -1,28 +1,49 @@
 import React from "react";
 import { v4 } from "uuid";
+import PropTypes from "prop-types";
 
 function NewKegForm(props) {
   function handleNewKegFromSubmission(event) {
     event.preventDefault();
-    console.log(event.target.name.value);
-    console.log(event.target.brand.value);
-    console.log(event.target.price.value);
+    props.onNewKegCreation({
+      name: event.target.name.value,
+      brand: event.target.brand.value,
+      price: event.target.price.value,
+      pintsLeft: event.target.pintsLeft.value,
+      alcoholContent: event.target.alcoholContent.value,
+      id: v4(),
+    });
   }
   return (
     <React.Fragment>
       <form onSubmit={handleNewKegFromSubmission}>
         <input type="text" name="name" placeholder="Keg Name" />
         <input type="text" name="brand" placeholder="Keg Brand" />
-        <input type="number" name="price" placeholder="Keg price per pint" />
+        <input
+          type="number"
+          name="price"
+          placeholder="Keg price per pint"
+          min="0"
+        />
+        <input
+          type="number"
+          name="pintsLeft"
+          placeholder="Pint amount in stock"
+          min="0"
+        />
         <input
           type="number"
           name="alcoholContent"
-          placeholder="Alcohol Content"
+          placeholder="Alcohol by volume"
         />
         <button type="submit">Add Keg</button>
       </form>
     </React.Fragment>
   );
 }
+
+NewKegForm.propTypes = {
+  onNewKegCreation: PropTypes.func,
+};
 
 export default NewKegForm;
