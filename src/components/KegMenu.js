d63dfeb80e38818b222267c8609953517502769a
TorchAblaze <tiffanygreathead@gmail.com>
2021-07-02 14:06:29 -0700

    Add Keg object and menu, update README

--- src/components/KegMenu.js
+++ src/components/KegMenu.js
@@ -1,6 +1,30 @@
 import React from "react";
 import Keg from "./Keg";
+import PropTypes from "prop-types";
 
-function KegMenu() {
-  return <Keg />;
+function KegMenu(props) {
+  return (
+    <React.Fragment>
+      <hr />
+      {props.kegMenu.map((keg) => (
+        <Keg
+          whenKegClicked={props.onKegSelection}
+          name={keg.name}
+          brand={keg.brand}
+          price={keg.price}
+          pintsLeft={keg.pintsLeft}
+          alcoholContent={keg.alcoholContent}
+          id={keg.id}
+          key={keg.id}
+        />
+      ))}
+    </React.Fragment>
+  );
 }
+
+KegMenu.propTypes = {
+  kegMenu: PropTypes.array,
+  onKegSelection: PropTypes.func,
+};
+
+export default KegMenu;
