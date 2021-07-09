b99e59dbe797f5d9744495254eda1ef5b496d4ca
TorchAblaze <tiffanygreathead@gmail.com>
2021-07-09 15:21:16 -0700

    Add passing add keg data test

--- src/reducers/keg-menu-reducer.js
+++ src/reducers/keg-menu-reducer.js
@@ -1,3 +1,18 @@
 export default (state = {}, action) => {
-  return state;
+  const { name, brand, price, pintsLeft, alcoholContent, id } = action;
+  switch (action.type) {
+    case "ADD_KEG":
+      return Object.assign({}, state, {
+        [id]: {
+          name: name,
+          brand: brand,
+          price: price,
+          pintsLeft: pintsLeft,
+          alcoholContent: alcoholContent,
+          id: id,
+        },
+      });
+    default:
+      return state;
+  }
 };
