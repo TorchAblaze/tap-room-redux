0cb70bc7243a1690729fa2b1ecbb62358f17b762
TorchAblaze <tiffanygreathead@gmail.com>
2021-07-12 23:48:19 -0700

    Add passing working ADD_KEG and TOGGLE_FORM actions test

--- src/__tests__/reducers/index-reducer.test.js
+++ src/__tests__/reducers/index-reducer.test.js
@@ -1,4 +1,9 @@
 import rootReducer from "../../reducers/index";
+import { createStore } from "redux";
+import formVisibleReducer from "../../reducers/form-visible-reducer";
+import kegMenuReducer from "../../reducers/keg-menu-reducer";
+
+let store = createStore(rootReducer);
 
 describe("rootReducer", () => {
   test("Should return default state if no action type is recognized", () => {
@@ -6,5 +11,42 @@ describe("rootReducer", () => {
       fullKegMenu: {},
       formVisibleOnPage: false,
     });
+  });
+
+  test("Check that initial state of kegMenuReducer matches root reducer", () => {
+    expect(store.getState().fullKegMenu).toEqual(
+      kegMenuReducer(undefined, { type: null })
+    );
+  });
+
+  test("Check that inital state of formVisibleReducer matches root reducer", () => {
+    expect(store.getState().formVisibleOnPage).toEqual(
+      formVisibleReducer(undefined, { type: null })
+    );
+  });
+
+  test("Check that ADD_KEG action works for kegMenuListReducer and root reducer", () => {
+    const action = {
+      type: "ADD_KEG",
+      name: "Rebel IPA",
+      brand: "Samuel Adams",
+      price: 16.0,
+      pintsLeft: 124,
+      alcoholContent: 6.5,
+      id: 1,
+    };
+    store.dispatch(action);
+    expect(store.getState().fullKegMenu).toEqual(
+      kegMenuReducer(undefined, action)
+    );
+  });
+  test("Check that TOGGLE_FORM action works for formVisible Reducer and root reducer", () => {
+    const action = {
+      type: "TOGGLE_FORM",
+    };
+    store.dispatch(action);
+    expect(store.getState().formVisibleOnPage).toEqual(
+      formVisibleReducer(undefined, action)
+    );
   });
 });
